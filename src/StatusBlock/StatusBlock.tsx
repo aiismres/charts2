28077729ff84446f4b0b47cb33d40be36118bdea
Aleksey Grachev <GrachevAI@gazpromenergosbyt.ru>
2023-08-10 11:29:19 +0300

    fix StatusBlock

--- src/StatusBlock/StatusBlock.tsx
+++ src/StatusBlock/StatusBlock.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './statusblock.module.css';
 
 export function StatusBlock() {
-  const serverStatus = JSON.parse(localStorage.getItem('isServerError') || 'j');
+  const serverStatus = JSON.parse(localStorage.getItem('isServerError') || '');
 
   return (
     <>
