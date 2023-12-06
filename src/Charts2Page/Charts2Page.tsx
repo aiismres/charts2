b16a8f8ab90372c395716f089cf3bba68fdf4246
Aleksey Grachev <GrachevAI@gazpromenergosbyt.ru>
2023-12-06 09:05:06 +0300

    delete unused code

--- src/Charts2Page/Charts2Page.tsx
+++ src/Charts2Page/Charts2Page.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import styles from './charts.module.css';
 import {
-  LineChart,
-  Line,
   ResponsiveContainer,
   BarChart,
   Bar,
@@ -15,14 +13,10 @@ import { getChartData, readChartsList } from '../fetchapi/fetchapi';
 import { ChartItem } from '../global';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Fade from '@mui/material/Fade';
 import { SwitchTransition, CSSTransition } from 'react-transition-group';
-import { Navigate } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { GRAPH_INTERVAL, TICK_FONT_SIZE } from '../globalConst';
-import { produce } from 'immer';
 import { StatusBlock } from '../StatusBlock';
-import { Layout } from '../Layout';
 // import './styles.css';
 
 export interface ChartData {
@@ -39,18 +33,10 @@ export function Charts2Page() {
   const [chartName, setChartName] = useState({ nameCh1: '', nameCh2: '' });
   const chartDataRef1 = useRef<ChartData[]>([]);
   const chartDataRef2 = useRef<ChartData[]>([]);
-  // const [isFetchFinish, setIsFetchFinish] = useState(false);
-  // const [isServerError, setIsServerError] = useState(false);
-  const [isServerRes, setIsServerRes] = useState(true);
-  // const [chartDataCache, setChartDataCache] = useState<Array<ChartData[]>>([]);
-  const navigate = useNavigate();
+  const chartsListRef = useRef<ChartItem[]>([]);
 
-  // if (!localStorage.chartDataCache) {
-  //   localStorage.setItem('chartDataCache', JSON.stringify([]));
-  // }
-  // if (!localStorage.chartListCache) {
-  //   localStorage.setItem('chartListCache', JSON.stringify([]));
-  // }
+  const [isServerRes, setIsServerRes] = useState(true);
+  const navigate = useNavigate();
 
   useLayoutEffect(() => {
     const charsList = JSON.parse(
@@ -74,12 +60,7 @@ export function Charts2Page() {
   useEffect(() => {
     const timer = setInterval(() => {
       console.log('setInterval', chartsList);
-      // setChartN((n) => (chartsList[n + 2] ? n + 2 : 0));
-      // if (!isServerRes) {
-      //   setIsServerError(true);
-      // } else {
-      //   setIsServerError(false);
-      // }
+
       if (chartsList[chartN + 2]) {
         console.log('if (chartsList[chartN + 2])', chartsList[chartN + 2]);
         setChartN((n) => n + 2);
@@ -88,12 +69,11 @@ export function Charts2Page() {
         navigate('/chartsumm');
       }
     }, GRAPH_INTERVAL);
-    // clearing interval
+
     return () => clearInterval(timer);
   }, [chartsList, chartN]);
 
   useEffect(() => {
-    // *****************
     const dateArr: string[] = [];
     for (let i = 0; i < 15; i++) {
       dateArr.push(
@@ -109,7 +89,6 @@ export function Charts2Page() {
     console.log({ dateArr });
     setXAxisData(dateArr);
 
-    // *****************
     let chartDataCache: Array<ChartData[]> = JSON.parse(
       localStorage.getItem('chartDataCache') || '[]'
     );
@@ -128,7 +107,6 @@ export function Charts2Page() {
       nameCh1: chartsList[chartN]?.name,
       nameCh2: chartsList[chartN + 1]?.name,
     }));
-    // *****************
 
     Promise.all([
       getChartData(chartsList[chartN]?.id),
@@ -136,42 +114,23 @@ export function Charts2Page() {
     ])
       .then((res) => {
         console.log('asdf', res);
-        // setChartData1(addDate(res[0], dateArr));
-        // setChartData2(addDate(res[1], dateArr));
+
         let chartDataCache: Array<ChartData[]> = JSON.parse(
           localStorage.getItem('chartDataCache') || '[]'
         );
         chartDataCache[chartN] = res[0];
         chartDataCache[chartN + 1] = res[1];
         localStorage.setItem('chartDataCache', JSON.stringify(chartDataCache));
-        // setIsServerError(false);
         setIsServerRes(true);
         localStorage.setItem('isServerError', JSON.stringify(''));
       })
       .catch((err) => {
         console.log('new Error12!!', err);
-        // setIsServerError(true);
         localStorage.setItem(
           'isServerError',
           JSON.stringify('server needs care')
         );
-
-        // let chartDataCache: Array<ChartData[]> = JSON.parse(
-        //   localStorage.getItem('chartDataCache') || '[]'
-        // );
-        // setChartData1(addDate(chartDataCache[chartN], dateArr));
-        // setChartData2(addDate(chartDataCache[chartN + 1], dateArr));
-      })
-      .finally(() => {
-        // setChartName((st) => ({
-        //   ...st,
-        //   nameCh1: chartsList[chartN]?.name,
-        //   nameCh2: chartsList[chartN + 1]?.name,
-        // }));
-        // setIsFetchFinish(true);
       });
-
-    // *****************
   }, [chartsList, chartN]);
 
   function addDate(arr: ChartData[], dateArr2: string[]) {
@@ -185,12 +144,7 @@ export function Charts2Page() {
   }
 
   return (
-    // <>
-    // <Layout>
     <Box padding={5}>
-      {/* <Typography variant='h4' align='center'>
-          {chartName.nameCh1}
-        </Typography> */}
       <SwitchTransition mode="out-in">
         <CSSTransition
           classNames="fade"
@@ -245,7 +199,6 @@ export function Charts2Page() {
         </CSSTransition>
       </SwitchTransition>
 
-      {/* <ResponsiveContainer width='100%' height='100%'> */}
       <ResponsiveContainer width="100%" height={400}>
         <BarChart
           width={1780}
@@ -269,10 +222,7 @@ export function Charts2Page() {
           <CartesianGrid />
         </BarChart>
       </ResponsiveContainer>
-      {/* <div>{isServerError && 'server needs care'}</div> */}
       <StatusBlock />
     </Box>
-    // </Layout>
-    // </>
   );
 }
